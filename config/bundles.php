102624ff1e654328e9981ffd1c3e99c7b7b04621
Simo Karpin <dev@w4d2.eu>
2018-04-19 23:13:57 +0300

    Symfony 4.0.8 base + redis support

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle::class => ['all' => true],
+];
